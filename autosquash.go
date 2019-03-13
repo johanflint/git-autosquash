b4947ad8e58cdfcd8e3c170c017ac9ede2b84ae4
Johan Flint <johan.flint@gmail.com>
2019-03-13 13:31:23 +0100

    Determine the upstream commit for git rebase

--- autosquash.go
+++ autosquash.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/plumbing/storer"
 )
 
+const fixupPrefix = "fixup!"
+
 func main() {
-	_, err := git.PlainOpenWithOptions(".", &git.PlainOpenOptions{DetectDotGit: true})
+	repository, err := git.PlainOpenWithOptions(".", &git.PlainOpenOptions{DetectDotGit: true})
 	if err == git.ErrRepositoryNotExists {
 		printf("No git repository found")
 		return
@@ -16,9 +21,52 @@ func main() {
 		return
 	}
 
-	printf("Repository found")
+	var upstreamCommit *object.Commit
+	autosquashCommitMessages := make([]string, 0)
+
+	log, err := repository.Log(&git.LogOptions{})
+	log.ForEach(func(commit *object.Commit) error {
+		if commit.NumParents() > 1 {
+			return storer.ErrStop
+		}
+
+		if strings.HasPrefix(commit.Message, fixupPrefix) {
+			messageWithoutPrefix := strings.TrimLeft(strings.TrimPrefix(commit.Message, fixupPrefix), " ")
+			autosquashCommitMessages = append(autosquashCommitMessages, messageWithoutPrefix)
+		}
+
+		if matchesAutosquashCommit(commit, autosquashCommitMessages) {
+			upstreamCommit = commit
+		}
+
+		return nil
+	})
+
+	if len(autosquashCommitMessages) == 0 {
+		printf("Nothing to autosquash: no commits found that start with %s", fixupPrefix)
+		return
+	}
+
+	upstreamParentCommit, err := upstreamCommit.Parent(0)
+	if err == object.ErrParentNotFound {
+		printf("Unable to find the parent commit of %s", upstreamCommit.Hash)
+		return
+	}
+
+	gitArguments := []string{"rebase", "--autosquash", "-i", fmt.Sprintf("%s", upstreamParentCommit.Hash)}
+	printf("Executing git %s", strings.Join(gitArguments, " "))
 }
 
 func printf(format string, args ...interface{}) {
 	fmt.Printf("%s\n", fmt.Sprintf(format, args...))
+}
+
+func matchesAutosquashCommit(commit *object.Commit, autosquashCommitMessages []string) bool {
+	for _, commitMessage := range autosquashCommitMessages {
+		if strings.Contains(commit.Message, commitMessage) {
+			return true
+		}
+	}
+
+	return false
 }
